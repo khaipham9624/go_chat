669718c6579dbff87939080d97ae7f7607131973
Khai Pham <khaipham9624@gmail.com>
2025-10-06 22:13:40 +0700

    refactory gui

--- src/server/main.go
+++ src/server/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"gochat/src/server/db"
 	"gochat/src/server/restserver"
 )
 
 func main() {
-	db.Init()
-	db.CreateUserIndex()
+	// db.Init()
+	// db.CreateUserIndex()
 	restserver.Start(8080)
 }
